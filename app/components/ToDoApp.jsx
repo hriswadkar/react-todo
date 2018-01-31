5a05e09b58f011be60b991fa0ae6b23f3a73b88b
hriswadkar <riswadkar.harshad@gmail.com>
2018-01-31 09:27:24 +0530

    Added Todo jsx and rendered all static todos on screen

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -1,10 +1,39 @@
 var React = require('react');
+var ToDoList = require('ToDoList');
 
 var ToDoApp = React.createClass({
+    getInitialState: function () {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: 'Walk the dog'
+                },
+                {
+                    id: 2,
+                    text: 'Clean the yard'
+                },
+                {
+                    id: 3,
+                    text: 'Learn ReactJS'
+                },
+                {
+                    id: 4,
+                    text: 'Create React World Clock app'
+                },
+                {
+                    id: 5,
+                    text: 'Plan to learn Node.js'
+                }
+            ]
+        };
+    },
     render: function () {
+        var {todos} = this.state;
+
         return (
             <div>
-                ToDoApp.jsx
+                <ToDoList todos={todos}/>
             </div>
         )
     }
