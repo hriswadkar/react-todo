f28ae482bb6e27f00adb06be1b305f30939b4c58
hriswadkar <riswadkar.harshad@gmail.com>
2018-02-10 13:43:04 +0530

    Updated todo to save the state in todo list

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var ToDoList = require('ToDoList');
 var AddToDo = require('AddToDo');
 var TodoSearch = require('TodoSearch');
+var uuid = require('node-uuid');
 
 var ToDoApp = React.createClass({
     getInitialState: function () {
@@ -10,30 +11,38 @@ var ToDoApp = React.createClass({
             searchText: '',
             todos: [
                 {
-                    id: 1,
+                    id: uuid(),
                     text: 'Walk the dog'
                 },
                 {
-                    id: 2,
+                    id: uuid(),
                     text: 'Clean the yard'
                 },
                 {
-                    id: 3,
+                    id: uuid(),
                     text: 'Learn ReactJS'
                 },
                 {
-                    id: 4,
+                    id: uuid(),
                     text: 'Create React World Clock app'
                 },
                 {
-                    id: 5,
+                    id: uuid(),
                     text: 'Plan to learn Node.js'
                 }
             ]
         };
     },
     handleAddToDo: function (text) {
-        alert('new todo: ' + text);
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                {
+                    id: uuid(),
+                    text: text
+                }
+            ]
+        });
     },
     handleSearch: function (showCompleted, searchText) {
         this.setState({
--- app/tests/components/ToDoApp.test.jsx
+++ app/tests/components/ToDoApp.test.jsx
@@ -10,4 +10,14 @@ describe('ToDoApp', () => {
     it('should exist', () => {
         expect(ToDoApp).toExist();
     });
+
+    it('should add todo to the todos state on handleAddTodo', () => {
+        var todoText = 'Test Add';
+        var todoApp = TestUtils.renderIntoDocument(<ToDoApp/>);
+
+        todoApp.setState({todos: []});
+        todoApp.handleAddToDo(todoText);
+
+        expect(todoApp.state.todos[0].text).toBe(todoText);
+    });
 });
