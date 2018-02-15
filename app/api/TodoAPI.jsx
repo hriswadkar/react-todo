b2c679bafc3866cedd32db878d824e59d1a8dd66
hriswadkar <riswadkar.harshad@gmail.com>
2018-02-15 20:57:41 +0530

    Added filter by searchText

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -28,8 +28,23 @@ module.exports = {
         });
 
         // filter by searchText
+        if (searchText.trim().length > 0) {
+            filteredTodos = filteredTodos.filter((todo) => {
+                var text = todo.text.toLowerCase();
+                return searchText.length === 0 || text.indexOf(searchText) > -1;
+            });            
+        }
 
         // sort todos with non-completed first
+        filteredTodos.sort((a, b) => {
+            if (a.completed  === false && b.completed === true) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
 
         return filteredTodos;
     }
